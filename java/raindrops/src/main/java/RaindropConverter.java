15251671521682147ae9090e5bde10ef51e499df
Cory Robinson <cearobinson@gmail.com>
2020-09-03 14:53:42 -0700

    Successfully solved this challenge. All unit tests passing.

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,21 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        final StringBuilder resultBuilder = new StringBuilder();
+
+        if (number % 3 == 0) {
+            resultBuilder.append("Pling");
+        }
+        if (number % 5 == 0) {
+            resultBuilder.append("Plang");
+        }
+        if (number % 7 == 0) {
+            resultBuilder.append("Plong");
+        }
+
+        final String result = resultBuilder.toString();
+
+        return result.equals("") ? Integer.toString(number) : result;
     }
 
 }
