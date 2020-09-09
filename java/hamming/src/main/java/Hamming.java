1fd61b66c5fba01e312c35b6bc620e323c37f6b6
Cory Robinson <cearobinson@gmail.com>
2020-09-09 12:20:23 -0700

    Completed hamming

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Hamming {
+    final String leftStrand;
+    final String rightStrand;
+
+    public Hamming(String leftStrand, String rightStrand) throws IllegalArgumentException {
+
+        if (leftStrand.equals("") && !rightStrand.equals(""))
+            throw new IllegalArgumentException("left strand must not be empty.");
+
+        else if (rightStrand.equals("") && !leftStrand.equals(""))
+            throw new IllegalArgumentException("right strand must not be empty.");
+
+        else if (leftStrand.length() != rightStrand.length())
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+
+        else {
+            this.leftStrand = leftStrand;
+            this.rightStrand = rightStrand;
+        }
+    }
+
+    public int getHammingDistance() {
+        int hammingDistance = 0;
+
+        if (leftStrand.equals(rightStrand))
+            return hammingDistance;
+
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i))
+                hammingDistance++;
+        }
+        return hammingDistance;
+    }
+}
